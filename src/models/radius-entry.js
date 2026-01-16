ae0a5dab670dbc98018d0e12dab64735eb5b8bc8
dev-grimnir <dev.grimnir@protonmail.com>
2026-01-16 15:29:45 -0500

    Update radius-entry.js

--- src/models/radius-entry.js
+++ src/models/radius-entry.js
@@ -1,1 +1,14 @@
+/**
+ * @file src/models/radius-entry.js
+ * @requires ../../core/utils   (only if you later add formatting methods here)
+ */
+class RadiusEntry {
+    constructor(timestamp, isStart) {
+        this.timestamp = timestamp;
+        this.isStart = isStart; // true = Start, false = Stop
+    }
 
+    getDate() {
+        return new Date(this.timestamp);
+    }
+}
