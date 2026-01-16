a9faa8608f6e0e99ed316cc2ac04b5a9c46e9839
dev-grimnir <dev.grimnir@protonmail.com>
2026-01-16 15:39:03 -0500

    Create neonova-radius-paginator.user.js

---
+++ src/scripts/neonova-radius-paginator.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         NeoNova RADIUS Search Paginator (Proof of Concept)
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  OOP classes to paginate through RADIUS search results and collect Start/Stop events
+// @author       dev-grimnir
+// @match        https://admin.neonova.net/*
+// @grant        none
+// @run-at       document-end
+// @updateURL    https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/scripts/neonova-radius-paginator.user.js
+// @downloadURL  https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/scripts/neonova-radius-paginator.user.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/core/utils.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/models/radius-entry.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/models/radius-metrics.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/collectors/radius-collector.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/analyzers/radius-analyzer.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/radius-report-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/radius-controller.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // All classes are now available globally via @require
+
+    const controller = new RadiusController();
+    controller.run();
+})();
