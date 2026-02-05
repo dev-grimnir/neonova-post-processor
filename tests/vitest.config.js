a519a94b29e9d411b8f8f5835f633b962bbc8e18
dev-grimnir <dev.grimnir@protonmail.com>
2026-02-04 22:12:36 -0500

    Update vitest.config.js

--- tests/vitest.config.js
+++ tests/vitest.config.js
@@ -1,9 +1,6 @@
+// vitest.config.js
 import { defineConfig } from 'vitest/config';
-import { fileURLToPath } from 'node:url';
 import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   test: {
