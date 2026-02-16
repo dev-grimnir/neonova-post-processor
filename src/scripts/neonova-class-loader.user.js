61a85695e9d48916d0ccb4cd807681740f410af8
dev-grimnir <dev.grimnir@protonmail.com>
2026-02-15 21:06:23 -0500

    Update neonova-class-loader.user.js

--- src/scripts/neonova-class-loader.user.js
+++ src/scripts/neonova-class-loader.user.js
@@ -11,6 +11,21 @@
 // @updateURL    https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/scripts/neonova-class-loader.user.js
 // @downloadURL  https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/scripts/neonova-class-loader.user.js
 // @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/base-neonova-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/neonova-analyzer.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/neonova-report-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/neonova-dashboard-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/neonova-report-order-controller.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/controllers/neonova-collector.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/base-neonova-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/neonova-dashboard-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/neonova-report-order-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/neonova-progress-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/views/neonova-report-view.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/models/customer.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/models/log-entry.js
+// @require      https://raw.githubusercontent.com/dev-grimnir/neonova-post-processor/main/src/core/utils.js
+
+
 
 // ==/UserScript==
 
@@ -25,7 +40,18 @@
     // Wait for all critical classes before starting the dashboard
     (function waitForDependencies() {
         // List all classes that must be defined before we proceed
-        if (typeof BaseNeonovaController !== 'undefined') {
+        if (typeof BaseNeonovaController !== 'undefined' &&
+            typeof NeonovaAnalyzer !== 'undefined' && 
+            typeof NeonovaCollector !== 'undefined' &&
+            typeof NeonovaDashboardController !== 'undefined' &&
+            typeof NeonovaReportOrderController !== 'undefined' &&
+            typeof Customer !== 'undefined' &&
+            typeof LogEntry !== 'undefined' &&
+            typeof BaseNeonovaView !== 'undefined' &&
+            typeof NeonovaDashboardView !== 'undefined' &&
+            typeof NeonovaProgressView !== 'undefined' &&
+            typeof NeonovaReportOrderView !== 'undefined' &&
+            typeof NeonovaReportView !== 'undefined') {
 
             console.log('All dependencies loaded');
 
