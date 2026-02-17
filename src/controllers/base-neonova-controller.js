1e40d7bfcfc027829ea6d4621a301d688a378e52
dev-grimnir <dev.grimnir@protonmail.com>
2026-02-16 19:55:23 -0500

    Update base-neonova-controller.js

--- src/controllers/base-neonova-controller.js
+++ src/controllers/base-neonova-controller.js
@@ -181,8 +181,12 @@ class BaseNeonovaController {
             }
 
             // Stop conditions
-            if (pageEntries.length < this.HITS_PER_PAGE) break;           // last page
-            if (knownTotal !== null && entries.length >= knownTotal) break; // reached total
+            if (pageEntries.length < this.HITS_PER_PAGE)
+                console.log("BaseNeonovaController.#fetchallLogPages() -> less entries than 100.  Ending pagination.");
+            break;           // last page
+            if (knownTotal !== null && entries.length >= knownTotal)
+                console.log("BaseNeonovaController.#fetchallLogPages() -> length is greater than or equal to known total.  Ending pagination.");
+            break; // reached total
 
             // Safety: prevent infinite loop if total never parsed
             if (page > 200) break;
