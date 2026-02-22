bd32d4ade33aa875002b3ae10c4dd31922f62eeb
dev-grimnir <dev.grimnir@protonmail.com>
2026-02-22 17:19:06 -0500

    Update neonova-http-controller.js

--- src/controllers/neonova-http-controller.js
+++ src/controllers/neonova-http-controller.js
@@ -138,6 +138,9 @@ class NeonovaHTTPController {
             const status = cells[4].textContent.trim();
             const sessionTime = cells[6].textContent.trim();
 
+            const dateObj = new Date(timestampStr + ' EST');
+            dateObj.setTime(dateObj.getTime() + (dateObj.getTimezoneOffset() * 60000) + (5 * 3600000));
+
             const dateObj = new Date(timestampStr);
             if (isNaN(dateObj.getTime())) return;
 
