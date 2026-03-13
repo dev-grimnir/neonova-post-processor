f86cb782ffe42669630853e8db5a399c63786fe8
dev-grimnir <dev.grimnir@protonmail.com>
2026-03-12 20:07:38 -0400

    Update neonova-dashboard.user.js

--- src/scripts/neonova-dashboard.user.js
+++ src/scripts/neonova-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NovaSubscriber - Dashboard
 // @namespace    http://tampermonkey.net/
-// @version      15.9
+// @version      15.10
 // @description  Real-time customer modem connection dashboard (separate script)
 // @author       dev-grimnir
 // @match        https://admin.neonova.net/*
