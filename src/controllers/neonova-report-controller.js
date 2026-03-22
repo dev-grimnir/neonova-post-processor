9b62f3fcbd3b9e05005f4825e35ccdf6899929cc
dev-grimnir <dev.grimnir@protonmail.com>
2026-03-22 18:23:59 -0400

    Update neonova-report-controller.js

--- src/controllers/neonova-report-controller.js
+++ src/controllers/neonova-report-controller.js
@@ -1,11 +1,25 @@
 class NeonovaReportController {
-    constructor() {
-        this.reportView = null;
-        this.username = null;
-        this.friendlyName = null;
-        this.metrics = null;
-        this.sanitizedEntries.length = null;
-        this metrics.longDisconnects = null;
+    constructor(username, friendlyName, metrics, length, longDisconnects) {
+        this.username = username;
+        this.friendlyName = friendlyName;
+        this.metrics = metrics;
+        this.length = sanitizedEntries.length;
+        this longDisconnects = metrics.longDisconnects;
         
+    }
+
+    generateAndOpen() {
+        const reportView = new NeonovaReportView(
+            this.username,
+            this.friendlyName,
+            this.metrics,
+            this.entryCount,
+            this.longDisconnects
+        );
+
+        const reportHTML = reportView.generateReportHTML('');
+        const newTab = window.open('', '_blank');
+        newTab.document.write(reportHTML);
+        newTab.document.close();
     }
 }
