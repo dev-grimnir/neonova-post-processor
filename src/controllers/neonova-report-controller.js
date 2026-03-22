ffd514c4b50c89a66bf2d9819330883919d5edb5
dev-grimnir <dev.grimnir@protonmail.com>
2026-03-22 17:07:30 -0400

    Update neonova-report-controller.js

--- src/controllers/neonova-report-controller.js
+++ src/controllers/neonova-report-controller.js
@@ -1,23 +1,11 @@
 class NeonovaReportController {
     constructor() {
-        this.progressView = new NeonovaProgressView();
-        this.collector = new NeonovaCollector();
-        this.reportView = null  
-        this.analyzer = null
-    }
-
-    async generateReportData(username, friendlyName, startDate, endDate = new Date(), onProgress = null) {
-        const entries = await this.paginateReportLogs(username, startDate, endDate, onProgress);
-        const cleaned = this.collector.cleanEntries(entries);
-        this.analyzer = new NeonovaAnalyzer(cleaned);
-        const metrics = this.analyzer.computeMetrics();
-        return {
-            username,
-            friendlyName,
-            period: { start: startDate, end: endDate },
-            entries,
-            cleanedEntries: cleaned,
-            metrics
-        };
+        this.reportView = null;
+        this.username = null;
+        this.friendlyName = null;
+        this.metrics = null;
+        this.sanitizedEntries.length = null;
+        this metrics.longDisconnects = null;
+        
     }
 }
