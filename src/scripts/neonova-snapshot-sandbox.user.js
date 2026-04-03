ab71df21eaa2a8edcc90b88d78c6186aeb3b2887
dev-grimnir <dev.grimnir@protonmail.com>
2026-04-03 13:23:12 -0400

    Update neonova-snapshot-sandbox.user.js

--- src/scripts/neonova-snapshot-sandbox.user.js
+++ src/scripts/neonova-snapshot-sandbox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NovaSubscriber - Snapshot
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Real-time customer modem connection dashboard (separate script)
 // @author       dev-grimnir
 // @match        https://admin.neonova.net/*
