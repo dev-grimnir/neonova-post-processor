ca5dce526c85393ca83a378af07c858dd8c0d535
dev-grimnir <dev.grimnir@protonmail.com>
2026-04-08 19:36:32 -0400

    Update neonova-report-controller.js

--- src/controllers/neonova-report-controller.js
+++ src/controllers/neonova-report-controller.js
@@ -15,8 +15,8 @@ class NeonovaReportController {
 
     openDailySnapshot(dateStr) {   // dateStr like "2026-03-22"
         const [year, month, day] = dateStr.split('-').map(Number);
-        const startDate = new Date(year, month - 1, day);
-        const endDate   = new Date(year, month - 1, day);
+        const startDate = new Date(year, month - 1, day, 0, 0, 0, 0);
+        const endDate   = new Date(year, month - 1, day, 23, 59, 59, 999);
     
         new NeonovaSnapshotController(
             this.model.username,
